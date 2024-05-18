7d7ae88331fc4b0fbfbc58a97f32774df61739ff
LeonardoSantosBR <leosantosmelo07@gmail.com>
2024-05-17 23:16:55 -0300

    feat: slider e alinhamento da tela principal

---
+++ app/index.styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F3FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 6,
+    padding: 8,
+  },
+  slider: {
+    height: 50,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text } from "react-native";
+import { styles } from "./index.styles";
+import Slider from "@react-native-community/slider";
 
 export default function App() {
-  return(
-    <View></View>
-  ) 
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../src/assets/images/logo.png")}
+        style={styles.logo}
+      />
+
+      <Text>20 caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={styles.slider}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="green"
+          minimumTrackTintColor="#414C98"
+          thumbTintColor="#392de9"
+        />
+      </View>
+    </View>
+  );
 }
