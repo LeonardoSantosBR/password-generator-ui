4166ad6686326eea8f30b32ac512b19f5d1d8a45
LeonardoSantosBR <leosantosmelo07@gmail.com>
2024-08-21 23:30:28 -0300

    refactor: inicio de refatoração da tela de criar conta

--- app/pages/auth/signup/signup.tsx
+++ app/pages/auth/signup/signup.tsx
@@ -1,21 +1,25 @@
+import { useForm, Controller } from "react-hook-form";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./signup.style";
 import { useDispatch } from "react-redux";
 import { setSign } from "../../../redux/signSlice.ts/signSlice";
 import { useMutation } from "@tanstack/react-query";
 import { createUser } from "@/app/services/users";
-import { useState } from "react";
+import { UsersFormData } from "@/app/types/users/users";
 
 export default function SignUp() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UsersFormData>();
 
   const { mutateAsync: createUserFn } = useMutation({
     mutationFn: createUser,
   });
 
-  async function handleSignUpUser(data: { email: string; password: string }) {
+  async function onSubmit(data: UsersFormData) {
     try {
       await createUserFn({
         email: data.email,
@@ -26,40 +30,75 @@ export default function SignUp() {
     }
   }
 
-  const data = {
-    email: email,
-    password: password,
-  };
-
   return (
     <View style={styles.container}>
       <Text style={styles.text}>👋 Crie sua conta</Text>
 
-      <View style={styles.formArea}>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          placeholder="Senha"
-          style={styles.input}
-          secureTextEntry={true}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <TextInput
-          placeholder="Confirmar senha"
-          style={styles.input}
-          secureTextEntry={true}
-        />
-      </View>
+      <Controller
+        control={control}
+        name="email"
+        rules={{ required: "Email obrigatório." }}
+        render={({ field: { value, onChange } }) => {
+          return (
+            <>
+              <TextInput
+                value={value}
+                placeholder="Email"
+                style={styles.input}
+                onChangeText={onChange}
+                autoCapitalize="none"
+              />
+            </>
+          );
+        }}
+      />
+
+      <Controller
+        control={control}
+        name="password"
+        rules={{ required: "Senha obrigatória." }}
+        render={({ field: { value, onChange } }) => {
+          return (
+            <>
+              <TextInput
+                value={value}
+                placeholder="Senha"
+                style={styles.input}
+                secureTextEntry={true}
+                onChangeText={onChange}
+                autoCapitalize="none"
+              />
+            </>
+          );
+        }}
+      />
+
+      <Controller
+        control={control}
+        name="confirmPassword"
+        rules={{ required: "Necessário confirmar a senha." }}
+        render={({ field: { value, onChange } }) => {
+          return (
+            <>
+              <TextInput
+                value={value}
+                placeholder="Confirmar senha"
+                style={styles.input}
+                secureTextEntry={true}
+                onChangeText={onChange}
+                autoCapitalize="none"
+              />
+            </>
+          );
+        }}
+      />
 
       <Text onPress={() => dispatch(setSign())} style={styles.newCount}>
         Já possui conta?
       </Text>
 
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText} onPress={() => handleSignUpUser(data)}>
+        <Text style={styles.buttonText} onPress={handleSubmit(onSubmit)}>
           Criar
         </Text>
       </TouchableOpacity>
